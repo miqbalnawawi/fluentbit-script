6e7982c395ecb5b2dc138abaacff038da3d79840
Muhammad Iqbal Nawawi <75749294+miqbalnawawi@users.noreply.github.com>
2024-02-15 15:27:01 +0700

    Update fluentbit.sh

--- fluentbit.sh
+++ fluentbit.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
+
 sudo apt update
 sudo apt install -y curl
+
 curl https://raw.githubusercontent.com/fluent/fluent-bit/master/install.sh | sh
-systemctl start fluent-bit.service
+
+sudo systemctl start fluent-bit.service
 
 sudo rm /etc/fluent-bit/fluent-bit.conf
 
-# Isi dari file fluent-bit.conf
 CONF_CONTENT=$(cat <<EOF
-#[INPUT]
-#    name              tail
-#    path              /var/log/*.log
-#    path_key         path
-
 [INPUT]
     Name              tail
     Path              /var/log/syslog
     Tag               logs.*
-
-#[INPUT]
-#    Name     syslog
-#    Listen   0.0.0.0
-#    Port     5140
-#    Parser   syslog-rfc3164
-#    Mode     tcp
 
 [INPUT]
     Name          exec
@@ -34,67 +24,51 @@ CONF_CONTENT=$(cat <<EOF
 [FILTER]
     Name          record_modifier
     Match         exec_ipaddress
-    Record        hostname ${HOSTNAME}
-    Record        ip_address "${ENV['CURL_OUTPUT']}"
+    Record        hostname \${HOSTNAME}
 
 [INPUT]
-    name cpu
-    tag  cpu.local
-    # Read interval (sec) Default: 1
-    interval_sec 10
+    Name          cpu
+    Tag           cpu.local
+    Interval_Sec   10
 
 [FILTER]
     Name          record_modifier
     Match         cpu.local
-    Record        hostname ${HOSTNAME}
-    Record        ip_address "${ENV['CURL_OUTPUT']}"
+    Record        hostname \${HOSTNAME}
 
 [INPUT]
-    name   mem
-    tag    memory
-    interval_sec 10
+    Name          mem
+    Tag           memory
+    Interval_Sec   10
 
 [FILTER]
     Name          record_modifier
     Match         memory
-    Record        hostname ${HOSTNAME}
-    Record        ip_address "${ENV['CURL_OUTPUT']}"
-
-#[INPUT]
-#    Name          exec
-#    Tag           exec_cpu
-#    Command       grep -i "CPU" /var/log/syslog
-#    Interval_Sec  1
-
-#[FILTER]
-#    Name   parser
-#    Match  exec
-#    Key_Name log
-#    Parser json
+    Record        hostname \${HOSTNAME}
 
 [SERVICE]
     Flush        10
     Parsers_File parsers.conf
 
 [OUTPUT]
-    name stdout
-    match *
+    Name         stdout
+    Match        *
 
 [OUTPUT]
-    name  http
-    match *
-    host kurawa-logs.digitalevent.id
-    port 9428
-    compress gzip
-    uri /insert/jsonline?_stream_fields=stream,path&_msg_field=log&_time_field=date
-    format json_lines
-    json_date_format iso8601
-    header AccountID 0
-    header ProjectID 0
+    Name         http
+    Match        *
+    Host         kurawa-logs.digitalevent.id
+    Port         9428
+    Compress     gzip
+    URI          /insert/jsonline?_stream_fields=stream,path&_msg_field=log&_time_field=date
+    Format       json_lines
+    Json_Date_Format iso8601
+    Header       AccountID 0
+    Header       ProjectID 0
 EOF
 )
 
 # Simpan isi file ke dalam direktori /etc/fluent-bit/fluent-bit.conf
 echo "$CONF_CONTENT" | sudo tee /etc/fluent-bit/fluent-bit.conf >/dev/null
 
-systemctl restart fluent-bit.service
+sudo systemctl restart fluent-bit.service
